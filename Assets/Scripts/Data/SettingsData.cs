de9c641a1258dcbb5786d50e56a00f6b145c4285
votizlov <35692043+votizlov@users.noreply.github.com>
2020-04-26 13:30:41 +0300

    Initial commit

---
+++ Assets/Scripts/Data/SettingsData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsData : ScriptableObject
+{
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode right;
+    public KeyCode left;
+}
---
+++ Assets/Scripts/Object/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private SettingsData settings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(settings.forward)){}
+        
+    }
+}
